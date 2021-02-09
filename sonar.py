72311d16af0dd7e6cc5c143b24290e1ef2763dc2
gerryfranchetti@gmail.com <gerryfranchetti@gmail.com>
2021-02-09 22:52:50 +0000

    added sonar.py

---
+++ sonar.py
@@ -0,0 +1,27 @@
+import RPi.GPIO as SONAR
+import time
+SONAR.setmode(SONAR.BCM)
+TRIG = 15
+ECHO = 14
+SONAR.setup(TRIG,SONAR.OUT)
+SONAR.output(TRIG,0)
+
+SONAR.setup(ECHO,SONAR.IN)
+time.sleep(0.1)
+## print "starting merasurement..."
+
+SONAR.output(TRIG,1)
+time.sleep(0.00001)
+SONAR.output(TRIG,0)
+
+while SONAR.input(ECHO) == 0:
+    pass
+start = time.time()
+
+while SONAR.input(ECHO) == 1:
+    pass
+stop = time.time()
+
+print (stop - start) * 17000
+
+SONAR.cleanup()
