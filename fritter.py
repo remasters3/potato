8d4895bec500b603c53e90ce6ba2b10f536d4ba8
gerryfranchetti@gmail.com <gerryfranchetti@gmail.com>
2021-03-15 21:19:09 +0000

    03/15/2021 21:19:09

--- fritter.py
+++ fritter.py
@@ -26,18 +26,45 @@ try:
         rdist = sonar.pingRight()
         
         if ldist > rdist and ldist > fdist and ldist > bdist and ldist > mindist and ldist < maxdist:
-            movment.TurnRight(0,power)
+            while ldist > rdist and ldist > fdist and ldist > bdist and ldist > mindist and ldist < maxdist:
+                movment.TurnRight(0,power)
+                fdist = sonar.pingFront()
+                bdist = sonar.pingRear()
+                ldist = sonar.pingLeft()
+                rdist = sonar.pingRight()
+                time.sleep(0.2)
+                
         elif rdist > ldist and rdist > fdist and rdist > bdist and rdist > mindist and rdist < maxdist:
-            movment.TurnLeft(0,power)
+            while rdist > ldist and rdist > fdist and rdist > bdist and rdist > mindist and rdist < maxdist:
+                movment.TurnLeft(0,power)
+                fdist = sonar.pingFront()
+                bdist = sonar.pingRear()
+                ldist = sonar.pingLeft()
+                rdist = sonar.pingRight()
+                time.sleep(0.2)
+                
         elif fdist > ldist and fdist > rdist and fdist > bdist and fdist > mindist and fdist < maxdist:
-            movment.forward(0,power)
+            while fdist > ldist and fdist > rdist and fdist > bdist and fdist > mindist and fdist < maxdist:
+                movment.forward(0,power)
+                fdist = sonar.pingFront()
+                bdist = sonar.pingRear()
+                ldist = sonar.pingLeft()
+                rdist = sonar.pingRight()
+                time.sleep(0.2)
+                
         elif bdist > ldist and bdist > rdist and bdist > fdist and bdist > mindist and bdist < maxdist:
-            movment.backwards(0,power)
+            while bdist > ldist and bdist > rdist and bdist > fdist and bdist > mindist and bdist < maxdist:
+                movment.backwards(0,power)
+                fdist = sonar.pingFront()
+                bdist = sonar.pingRear()
+                ldist = sonar.pingLeft()
+                rdist = sonar.pingRight()
+                time.sleep(0.2)
         else:
             movment.allstop(0)
         system('clear')
         print(" | Front:{0} | Rear:{1} | Left:{2} | Right:{3} |".format(fdist,bdist,ldist,rdist))
-        time.sleep(0.2)
+        # time.sleep(0.2)
 finally:
     movment.servoa (90)
     movment.servob (90)
